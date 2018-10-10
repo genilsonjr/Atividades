2c993993ca8a741e77d096595293f3e96899b7ce
Genilson <gjsj@cesar.school>
2018-10-09 21:33:56 -0300

    Atividade

---
+++ questao_1.py
@@ -0,0 +1,30 @@
+## QUESTÃO 1 ## 
+# Faça um programa que calcule o aumento de um salário. Ele deve solicitar o 
+# valor do salário e a porcentagem do aumento. Exiba o valor do aumento e do 
+# novo salário. 
+##
+
+##
+# A sua resposta da questão deve ser desenvolvida dentro da função main()!!! 
+# Deve-se substituir o comado print existente pelo código da solução.
+# Para a correta execução do programa, a estrutura atual deve ser mantida,
+# substituindo apenas o comando print(questão...) existente.
+##
+<<<<<<< HEAD
+def main(salario= float(input('Qual e o seu salário atual: '))# pedir pro usuario por o salari atual.
+porcentagem= float(input('Quantos porcentos de aumento: '))#pedir a prcentagem do aumento.
+por= (salario * porcentagem)/100#calculo da porcentagem
+resutado= por + salario# culculo da soma do salario com a porcentagem
+print('O acrecimo foi de', por,'o seu salário vai ser', resutado,))#impressao na tela o resutado.
+ 
+=======
+def main()
+print("questao 1") 
+>>>>>>> parent of a83211a... Update questao_1.py
+
+
+    
+
+
+if __name__ == '__main__':
+    main()
